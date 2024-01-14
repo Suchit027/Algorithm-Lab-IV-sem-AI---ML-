5a88132bc53386569cee5e6718d5c4a30aeca316
Suchit Gupta <suchitgupta027@gmail.com>
2024-01-15 02:01:29 +0530

    corrections of middle school gcd method

--- lab2/q2.c
+++ lab2/q2.c
@@ -6,12 +6,34 @@ int *sieve(int a)
     arr[0] = arr[1] = -1;
     for (int i = 2; i < a + 1; ++i)
     {
-        for (int j = i * 2; j < a + 1; j += i)
+        if (arr[i] == 0)
         {
-            arr[j] = -1;
+            for (int j = i * 2; j < a + 1; j += i)
+            {
+                arr[j] = -1;
+            }
         }
     }
     return arr;
+}
+int power(int a, int b)
+{
+    int ans = 1;
+    for (; b > 0; --b)
+    {
+        ans *= a;
+    }
+    return ans;
+}
+int count(int prime, int num)
+{
+    int ans = 0;
+    while (num > 0 && num % prime == 0)
+    {
+        ans += 1;
+        num = num / prime;
+    }
+    return ans;
 }
 int gcd(int a, int b)
 {
@@ -21,7 +43,7 @@ int gcd(int a, int b)
     {
         if (a_arr[i] == 0 && a % i == 0 && b % i == 0)
         {
-            ans *= i;
+            ans *= power(i, count(i, a));
         }
     }
     return ans;
@@ -31,5 +53,7 @@ void main()
     printf("enter a and b\n");
     int a, b;
     scanf("%d %d", &a, &b);
-    printf("%d", gcd(a, b));
+    int x = a > b ? b : a;
+    int y = a > b ? a : b;
+    printf("%d", gcd(x, y));
 }
