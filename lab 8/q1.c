7b41437565c5782fae36827f6ebcbcc2b0c79895
Suchit Gupta <suchitgupta027@gmail.com>
2024-02-25 15:12:21 +0530

    top down heap

--- lab 8/q1.c
+++ lab 8/q1.c
@@ -3,47 +3,73 @@
 typedef struct heap *hl;
 struct heap
 {
-    int *arr;
-    int size, heapsize;
+    int *arr, size, heapsize;
 };
 hl create(int size)
 {
     hl ob = (hl)malloc(sizeof(struct heap));
-    ob->arr = (int *)malloc(sizeof(int) * size);
-    ob->size = size;
+    ob->size = size + 1;
+    ob->arr = (int *)malloc(sizeof(int) * size + 1);
     ob->heapsize = 0;
     return ob;
 }
-void push(hl ob, int n)
+void heap_push(hl heap, int ele)
 {
     int i = 1;
-    ob->arr[++ob->heapsize] = -10000;
-    while (i <= ob->heapsize)
+    while (i <= heap->heapsize)
     {
-        if (n > ob->arr[i])
+        if (heap->arr[i] > ele)
         {
-            int temp = ob->arr[i];
-            ob->arr[i] = n;
-            n = temp;
+            int temp = heap->arr[i];
+            heap->arr[i] = ele;
+            ele = temp;
         }
         i *= 2;
-        if (i < ob->heapsize && ob->arr[i] > ob->arr[i + 1])
+        if (i < heap->heapsize && heap->arr[i] > heap->arr[i + 1])
         {
             i += 1;
         }
     }
+    heap->arr[++heap->heapsize] = ele;
     return;
+}
+int pop(hl ob)
+{
+    if (ob->heapsize == 0)
+    {
+        return -1;
+    }
+    int val = ob->arr[1];
+    int temp = ob->arr[ob->heapsize--];
+    int parent = 1, child = 2;
+    while (child <= ob->heapsize) // note the <=
+    {
+        if ((ob->arr[child] > ob->arr[child + 1]) && (child < ob->heapsize))
+        {
+            child += 1;
+        }
+        if (temp <= ob->arr[child])
+        {
+            break;
+        }
+        ob->arr[parent] = ob->arr[child];
+        parent = child;
+        child *= 2;
+    }
+    ob->arr[parent] = temp;
+    return val;
 }
 void main()
 {
-    hl ob = create(6);
-    push(ob, 5);
-    push(ob, 3);
-    push(ob, 6);
-    push(ob, 2);
-    push(ob, 7);
-    for (int i = 1; i <= ob->heapsize; ++i)
+    hl heap = create(6);
+    heap_push(heap, 3);
+    heap_push(heap, 10);
+    heap_push(heap, 12);
+    heap_push(heap, 8);
+    heap_push(heap, 2);
+    heap_push(heap, 14);
+    for (int i = 0; i < 6; ++i)
     {
-        printf("%d ", ob->arr[i]);
+        printf("%d ", pop(heap));
     }
 }
